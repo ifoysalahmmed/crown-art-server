e7ed937bc8683a19124cd7080ab9f9e98ccd2249
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-06-08 17:14:06 +0600

    added users get api

--- index.js
+++ index.js
@@ -42,6 +42,11 @@ async function run() {
 
     // <---users collections apis--->
 
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
 
